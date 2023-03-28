2fe5dfab4d51e90dd8cb0a85e25c081d8bec64c5
bekzatkazatov <bekzatx2x@gmail.com>
2023-03-28 11:31:15 +0600

    API

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 const row = document.querySelector('.row')
 const continent = 'europe'
-const converter =document.querySelector('.converter')
+const input =document.querySelector('.input')
 const submit = document.querySelector('.submit')
 const title = document.querySelector('.title')
-const title2 = document.querySelector('.title2')
+const subtitle = document.querySelector('.subtitle')
 const title3 = document.querySelector('.title3')
 
 
 submit.addEventListener('click',() => {
-    let f = converter.value
-    fetch(`https://api.exchangerate.host/latest?base=KGS&symbols=USD,EUR,RUB,&amount=${f}`)
+    let a = input.value
+    fetch(`https://api.exchangerate.host/latest?base=KGS&symbols=USD,EUR,RUB,&amount=${a}`)
         .then(response => response.json())
         .then(data =>{
             console.log(data.rates)
-
-
-
-            title.innerHTML = Object.values(data.rates)[0].toFixed(4)
-            title2.innerHTML = Object.values(data.rates)[1].toFixed(4)
-            title3.innerHTML = Object.values(data.rates)[2].toFixed(4)
+            title.innerHTML = Object.values(data.rates)[0].toFixed(3)
+            subtitle.innerHTML = Object.values(data.rates)[1].toFixed(3)
+            title3.innerHTML = Object.values(data.rates)[2].toFixed(3)
         })
 })
 
@@ -36,7 +33,7 @@ fetch(`https://restcountries.com/v3.1/region/${continent}`)
    <div>lauguages: <h4>${Object.values (country.languages)}</h4></div>
     <div>Population: <h4>${country.population}</h4></div>
     <div>Area: <h4>${country.area}</h4></div>
-     <form action="${country.maps.googleMaps}" target="_blank">Maps:
+     <form action="${country.maps.googleMaps}" target="_blank">Location:
    <button>Переход по ссылке</button>
   </form>
     
